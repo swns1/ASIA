213a6309295a73fe33fe43844ce73ca7815a3e44
swns1 <seanwesleysalapare@gmail.com>
2026-05-06 13:03:07 +0800

    trying new branch

--- backend/identity-service/identity_service/settings.py
+++ backend/identity-service/identity_service/settings.py
@@ -118,6 +118,10 @@ DATABASES = {
     }
 }
 
+AUTHENTICATION_BACKENDS = [
+    "axes.backends.AxesStandaloneBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
